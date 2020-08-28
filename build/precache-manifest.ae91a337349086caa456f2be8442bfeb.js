9bb36547e81bdc0f175ee18a9164df6243c3ec43
khemrjneupane <61009988+khemrjneupane@users.noreply.github.com>
2020-08-28 17:35:42 +0300

    Add files via upload

---
+++ build/precache-manifest.ae91a337349086caa456f2be8442bfeb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a6b5355a839c6f967930f2a421907b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e870ec23558bbec0d4d",
+    "url": "/static/css/main.82c2a0be.chunk.css"
+  },
+  {
+    "revision": "7ff5c0841765514a9eb5",
+    "url": "/static/js/2.804aadc7.chunk.js"
+  },
+  {
+    "revision": "8a9a2ae4a25f202f52818f8f8074424e",
+    "url": "/static/js/2.804aadc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e870ec23558bbec0d4d",
+    "url": "/static/js/main.2b7d627d.chunk.js"
+  },
+  {
+    "revision": "698e2273aa8cdc8fc3f4",
+    "url": "/static/js/runtime-main.3c98804c.js"
+  }
+]);
